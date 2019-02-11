5dc80cab94481b3932cdb1e8363689d72e7912d0
skatpgusskat <skatpgusskat@naver.com>
2019-02-12 00:55:28 +0900

    Add type-generator

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ErrorCode } from './generated/ErrorCode';
+
+console.log(ErrorCode.DefaultErrorCode.InternalServerError);
 
 ReactDOM.render(<App />, document.getElementById('root'));
