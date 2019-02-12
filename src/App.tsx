c167b958fbdeb04a11c07df635fd5c34ef49ce46
skatpgusskat <skatpgusskat@naver.com>
2019-02-12 23:36:15 +0900

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logoSvg from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 export default function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logoSvg} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-           and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" component={MainPage} />
+    </Router>
   );
 }
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MainPage(): JSX.Element {
+  return (
+    <div>
+      i am MainPage, my father is Namse.
+    </div>
+  );
+}
