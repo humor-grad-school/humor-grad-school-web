ea0503da49139750bbfffdec542cafed6545aee1
skatpgusskat <skatpgusskat@naver.com>
2019-02-15 02:08:49 +0900

    Add simple post view page example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './components/MainPage';
+import PostViewPage from './components/PostViewPage';
 
 export default function App(): JSX.Element {
   return (
     <Router>
-      <Route path="/" component={MainPage} />
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/postView" component={PostViewPage} />
+      </Switch>
     </Router>
   );
 }
