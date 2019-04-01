60492329b4edae35c4895aaa4fb3a70a29329f87
bigfoodK <satcom1600@hanmail.net>
2019-04-02 01:04:10 +0900

    Add comment navigator

---
+++ src/components/PostView/CommentViewComponent/CommentNavigatorComponent.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type PageButtonProps = {
+  iscurrentpage: 'true' | 'false';
+}
+
+const Container = styled.div`
+  margin: 1em auto;
+  display: flex;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  border-radius: 2em;
+  background-color: #DDD;
+`;
+
+const PageButton = styled.button`
+  width: 3em;
+  height: 3em;
+  padding: 1em;
+  line-height: 1em;
+  text-align: center;
+  border-radius: 2em;
+  display: inline-block;
+  border: 0px;
+  vertical-align: middle;
+  color: ${(props: PageButtonProps) => (props.iscurrentpage === 'true' ? '#FFF' : '#000')}
+  ${(props: PageButtonProps) => (props.iscurrentpage === 'true' ? 'background-color: #AAA' : '')}
+  transition: background-color 0.25s, color 0.25s;
+  cursor: pointer;
+  :hover {
+    background-color: #CCC;
+  }
+`;
+
+export default function CommentNavigatorComponent({
+  pageNumber,
+  maxPageNumber,
+  changePageNumber,
+}: {
+  pageNumber: number;
+  maxPageNumber: number;
+  changePageNumber: (pageNumber: number) => void;
+}): JSX.Element {
+  const start = (pageNumber > 4)
+    ? pageNumber - 4
+    : 1;
+  const end = Math.min((pageNumber + 4), maxPageNumber);
+
+  const pageButtonElements: JSX.Element[] = [];
+
+  for (let i = start; i <= end; i += 1) {
+    pageButtonElements.push(
+      <PageButton
+        iscurrentpage={i === pageNumber ? 'true' : 'false'}
+        key={`comment-navigator-page-button-${i}`}
+        onClick={() => { changePageNumber(i); }}
+      >
+        {i}
+      </PageButton>,
+    );
+  }
+
+  return (
+    <Container>
+      <Wrapper>
+        {pageButtonElements}
+      </Wrapper>
+    </Container>
+  );
+}
--- src/components/PostView/CommentViewComponent/CommentViewComponent.tsx
+++ src/components/PostView/CommentViewComponent/CommentViewComponent.tsx
@@ -2,6 +2,7 @@ import React, { Component, ReactNode } from 'react';
 import styled from 'styled-components';
 import { CommentInfoes } from '../../../types/CommentData';
 import CommentComponent from './CommentComponent';
+import CommentNavigatorComponent from './CommentNavigatorComponent';
 
 type CommentViewComponentProps = {
   comments: CommentInfoes;
@@ -9,7 +10,12 @@ type CommentViewComponentProps = {
 
 type CommentViewComponentState = {
   isWriting: boolean;
+  commentPageNum: number;
 }
+
+const Container = styled.div`
+  padding-bottom: 8ex;
+`;
 
 const CommentList = styled.ul`
   list-style-type: none;
@@ -17,23 +23,53 @@ const CommentList = styled.ul`
   padding: 0px;
 `;
 
+function sliceComments(comments: CommentInfoes, pageNumber: number): CommentInfoes {
+  const start = (pageNumber - 1) * 20;
+  const end = pageNumber * 20;
+  const slicedComments = comments.slice(start, end);
+  return slicedComments;
+}
+
+function getMaxPageNumber(commentAmount: number): number {
+  return Math.ceil(commentAmount / 20);
+}
+
 export default class CommentViewComponent
   extends Component<CommentViewComponentProps, CommentViewComponentState> {
   public constructor(props: CommentViewComponentProps) {
     super(props);
+
+    const { comments } = this.props;
+
+    this.maxPageNumber = getMaxPageNumber(comments.length);
+
     this.state = {
       isWriting: false,
+      commentPageNum: this.maxPageNumber,
     };
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+
+  private maxPageNumber: number;
+
+  public handlePageChange(pageNumber: number): void {
+    this.setState({
+      commentPageNum: pageNumber,
+    });
   }
 
   public render(): ReactNode {
-    // TODO: Pagenation
     const {
       comments,
     } = this.props;
+    const {
+      commentPageNum,
+    } = this.state;
 
-    const commentComponents = comments.map((commentInfo) => {
-      const key = `comment-component-${commentInfo.writer.username}-${commentInfo.createdAt}`;
+    const slicedComments = sliceComments(comments, commentPageNum);
+
+    const commentComponents = slicedComments.map((commentInfo) => {
+      const key = `comment-component-${commentInfo.writer.username}-${commentInfo.createdAt}${Math.random()}`;
       return (
         <CommentComponent
           commentInfo={commentInfo}
@@ -48,12 +84,22 @@ export default class CommentViewComponent
 
     return (
       // TODO: header
-      // TODO: navigator
-      <CommentList>
-        {commentComponents}
-        {isWriting ? 'im writing' : 'im just watching'}
-      </CommentList>
-      // TODO: navigator
+      <Container>
+        <CommentNavigatorComponent
+          pageNumber={commentPageNum}
+          maxPageNumber={this.maxPageNumber}
+          changePageNumber={this.handlePageChange}
+        />
+        <CommentList>
+          {commentComponents}
+          {isWriting ? 'im writing' : 'im just watching'}
+        </CommentList>
+        <CommentNavigatorComponent
+          pageNumber={commentPageNum}
+          maxPageNumber={this.maxPageNumber}
+          changePageNumber={this.handlePageChange}
+        />
+      </Container>
     );
   }
 }
