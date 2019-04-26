e5a4ac3e16e466fafbef70002cecf5e4be64aee6
bigfoodK <satcom1600@hanmail.net>
2019-04-26 21:05:43 +0900

    Make custom facebook login button to login manually

--- src/components/Login/ThirdPartyLogin/FacebookLogin/FacebookLoginComponent.tsx
+++ src/components/Login/ThirdPartyLogin/FacebookLogin/FacebookLoginComponent.tsx
@@ -1,40 +1,84 @@
 import React from 'react';
+import styled from 'styled-components';
 import loadFacebookSdk from './loadFacebookSdk';
 import BaseLoginComponent from '../BaseLoginComponent';
 
 declare let FB: any;
 
+const FacebookLoginButton = styled.div`
+  display: inline-block;
+  width: 222px;
+  height: 49px;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: #3b5998;
+  color: #FFF;
+  line-height: 49px;
+`;
+
 export default class FacebookLoginComponent extends BaseLoginComponent<{}, {}> {
   public origin: string = 'facebook';
+
+  public isSdkLoaded: boolean = false;
+
+  private handleLoginResponse(response: any): void {
+    switch (response.status) {
+      case 'connected': {
+        this.onThirdPartyLoginSuccessful({
+          idToken: response.authResponse.accessToken,
+        });
+        break;
+      }
+
+      case 'not_authorized':
+      case 'unknown': {
+        this.onThirdPartyLoginFailed();
+        break;
+      }
+
+      default: {
+        this.onThirdPartyLoginFailed();
+        throw new Error(response);
+      }
+    }
+  }
 
   public componentDidMount(): void {
     loadFacebookSdk().then(() => {
       console.log('facebook loaded');
+      this.isSdkLoaded = true;
       FB.getLoginStatus((response: any) => {
-        console.log(response);
-        if (response.status === 'connected') {
-          this.onThirdPartyLoginSuccessful({
-            idToken: response.authResponse.accessToken,
-          });
-        } else {
-          console.error(response);
-          this.onThirdPartyLoginFailed();
-        }
+        this.handleLoginResponse(response);
       });
+    });
+  }
+
+  public handleLoginButtonClick(): void {
+    if (!this.isSdkLoaded) return;
+
+    if (!this.isLoginFinished) return;
+
+    if (this.isLoginSuccessful) {
+      this.login();
+      return;
+    }
+
+    FB.login((response: any) => {
+      this.handleLoginResponse(response);
+
+      if (!(this.isLoginFinished && this.isLoginSuccessful)) return;
+
+      this.login();
     });
   }
 
   public render(): JSX.Element {
     return (
-      <div
-        className="fb-login-button"
-        data-max-rows="1"
-        data-size="large"
-        data-button-type="continue_with"
-        data-show-faces="false"
-        data-auto-logout-link="false"
-        data-use-continue-as="false"
-      />
+      <FacebookLoginButton
+        onClick={() => { this.handleLoginButtonClick(); }}
+      >
+        페이스북계정으로 로그인
+      </FacebookLoginButton>
     );
   }
 }
