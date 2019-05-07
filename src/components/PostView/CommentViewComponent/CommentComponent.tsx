c1e84cbbb93555719a383f44647e2ea3d46767e4
bigfoodK <satcom1600@hanmail.net>
2019-05-07 19:22:31 +0900

    Change 'true' and 'false' string to real boolean

--- src/components/PostView/CommentViewComponent/CommentComponent.tsx
+++ src/components/PostView/CommentViewComponent/CommentComponent.tsx
@@ -20,12 +20,12 @@ type CommentComponentStates = {
 }
 
 type ContainerProps = {
-  issub: 'true' | 'false';
+  issub: boolean;
 }
 
 const Container = styled.li`
   margin-bottom: 1em;
-  margin-left: ${(props: ContainerProps) => (props.issub === 'true' ? '2em' : '')}
+  margin-left: ${(props: ContainerProps) => (props.issub ? '2em' : '')}
 `;
 
 const ParentCommentWriter = styled.span`
@@ -90,7 +90,7 @@ export default class CommentComponent
     } = this.state;
 
     return (
-      <Container issub={parentComment ? 'true' : 'false'}>
+      <Container issub={!!parentComment}>
         <CommentHeaderComponent
           commentInfo={commentInfo}
           postWriterId={postWriterId}
